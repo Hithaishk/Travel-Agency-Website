163a9605f56a17e4a37cba6c4c788df5036c89e8
Hithaish <96730035+Hithaishk@users.noreply.github.com>
2023-10-26 23:12:37 +0530

    Update index.js privacy

--- Day-7/index.js
+++ Day-7/index.js
@@ -12,7 +12,7 @@ const app=express()
 //DB Connection
 
 mongoose.connect(
-    "mongodb+srv://hithaish:Hithaish%40123@cluster0.larynjs.mongodb.net/"
+    "mongodb+srv://hithaish:%40123@cluster0.larynjs.mongodb.net/"
 ).then(()=>{
     console.log("Database is connected successfully😎")
 })
